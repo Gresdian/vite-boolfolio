35fb47afd76080b2a685c61867109a7bbe83d92a
Gresdian <chris_da98@outlook.it>
2024-03-05 20:04:53 +0100

    Milestone 3 completed

---
+++ src/components/Projects.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'Projects',
+    data() {
+        return{
+            projects:[]
+        }
+
+    }
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="">
+    
+</style>
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { reactive } from vuePlugin;
+
+export const store = reactive({
+    baseurl:'http://http://127.0.0.1:8000/'
+})
