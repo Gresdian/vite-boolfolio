20f7813ed77183aed101ae5504c77f8042490a3b
Gresdian <chris_da98@outlook.it>
2024-03-08 19:55:53 +0100

    Milestone 4-5 finished

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,20 +1,45 @@
 <script>
+import { store } from '../store';
 export default {
     name: 'SingleProject',
     props: {
         project: Object
     },
+    data(){
+        return {
+            store
+        }
+    },
+    methods: {
+        createExcerpt() {
+            return this.project.description.substr(0, 20) + '...';
+        },
+        getUrlImage(){
+            if(this.project.cover_image != null) {
+                image= '/storage/' + this.post.cover_image;
+            }
+            else {
+                image = '/img/aaaa.jpg';
+            }
+            return `${this.store.baseurl}/storage/${image}`
+        }
+    }
 
 }
 </script>
 <template lang="">
-    <div class="card">
-        <img :src="project.cover_image" alt="" class="card-img-top">
-        <div class="card-body">
-            <h4>
-                {{ project.name }}
-            </h4>
+    <div class="col-12 col-md-3">
+        <div class="card m-3">
+            <img :src="getUrlImage()" alt="">
+            <img :src="project.cover_image != null ? `${store.baseurl}/storage/${project.cover_image}` : `${store.baseurl}/storage/aaaa.jpg`" alt="" class="card-img-top">
+            <div class="card-body">
+                <h4>
+                    {{ project.name }}
+                </h4>
+                <p>{{ createExcerpt() }}</p>
+            </div>
         </div>
+
     </div>
 </template>
 <style lang="">
